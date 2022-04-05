d13888d467a7af93ed040ff3801726420bd67ebc
George Sims <gsims@softwareinstitute.com>
2022-04-05 10:53:06 +0100

    Sieve V2

--- sieve/src/sieve/sieve.cpp
+++ sieve/src/sieve/sieve.cpp
@@ -28,4 +28,35 @@ namespace sieve
         return ans;
     }
 
+    vector<bool> listOfPrimes(int range)
+    {
+        vector<bool> is_prime_list(range + 1, true);
+        is_prime_list[0] = false;
+        is_prime_list[1] = false;
+
+        for (int div = 2; div <= range; ++div)
+        {
+            if (!is_prime_list[div])
+                continue;
+            for (int dividend = div + div; dividend <= range; dividend += div)
+            {
+                is_prime_list[dividend] = false;
+            }
+        }
+        return is_prime_list;
+    }
+
+    // based on community submissions from exercism
+    vector<int> primesV2(int range)
+    {
+        vector<bool> is_prime_list = listOfPrimes(range);
+        vector<int> output_list;
+        for (int i = 0; i <= range; ++i)
+        {
+            if (is_prime_list[i])
+                output_list.push_back(i);
+        }
+        return output_list;
+    }
+
 } // namespace sieve
--- sieve/src/sieve/sieve.h
+++ sieve/src/sieve/sieve.h
@@ -6,4 +6,6 @@ namespace sieve
 {
     vector<int> primeMultiplesUpTo(int prime, int lim);
     vector<int> primes(int range);
+    vector<bool> listOfPrimes(int range);
+    vector<int> primesV2(int range);
 } // namespace sieve
